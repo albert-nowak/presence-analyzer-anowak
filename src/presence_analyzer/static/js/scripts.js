6e62824fd9c391346893a88c2a1b9fca575ee6d8
anowak <albertnowak12@gmail.com>
2017-04-10 09:17:58 +0200

    Added timeline + tests (fixed)

---
+++ src/presence_analyzer/static/js/scripts.js
@@ -0,0 +1,10 @@
+function parseInterval(value) {
+    var result = new Date(1,1,1);
+    result.setMilliseconds(value*1000);
+    return result;
+}
+
+function parseToDate(value) {
+    var result = new Date(value);
+    return result
+}
